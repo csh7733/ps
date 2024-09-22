579c2ba7cb9ab86999f2a923f1c869dc28e21b99
최성현 <149491102+csh7733@users.noreply.github.com>
2024-09-23 00:09:14 +0900

    [level 2] Title: 연속된 부분 수열의 합, Time: 78.85 ms, Memory: 130 MB -BaekjoonHub

--- 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.java
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.java
@@ -1,28 +1,39 @@
 class Solution {
+
+    private int findRight(int[] sum,int start,int end,int k){
+        int left = start;
+        int right = end;
+        int total = 0;
+        while(left<=right){
+            int mid = (left+right)/2;
+            total = sum[mid+1] - sum[start];
+            if(total < k) left = mid + 1;
+            if(total > k) right = mid - 1;
+            if(total == k) {
+                return mid;
+            }
+        }
+        return -1;
+    }
     public int[] solution(int[] sequence, int k) {
         int length = sequence.length;
-        int left = 0,right = 0,size = 1;
+        int[] sum = new int[length+1];
+        for(int i=1;i<=length;i++){
+            sum[i] = sum[i-1] + sequence[i-1];
+        }
+        int size = Integer.MAX_VALUE;
         int[] result = new int[2];
-        int sum = sequence[0];
-        int ansSize = Integer.MAX_VALUE;
-        while(right < length){
-            if(sum == k){
-                if(size < ansSize){
-                    ansSize = size;
+        for(int i=0;i<length;i++){
+            int left = i;
+            int right = findRight(sum,left,length-1,k);
+            if(right != -1){
+                if(right - left + 1 < size){
+                    size = right -left + 1;
                     result[0] = left;
                     result[1] = right;
                 }
-                right++;
-                if(right < length) sum += sequence[right];
-                size++;
-            }else if(sum < k){
-                right++;
-                if(right < length) sum += sequence[right];
-                size++;
-            }else if(sum > k){
-                sum -= sequence[left++];
-                size--;
             }
+
         }
         return result;
     }
